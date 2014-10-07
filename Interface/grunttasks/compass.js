1b6b0de023a70f6a22d5c849419b62c32103d100
soerensigfusson <soerensigfusson@me.com>
2014-10-07 15:47:38 +0200

    compasses

--- Interface/grunttasks/compass.js
+++ Interface/grunttasks/compass.js
@@ -13,5 +13,16 @@ module.exports = {
         options: {
             config: '<%= package.configs %>/compass/config-prototype.rb'
         }
-    }
+    },
+    prototype2:{
+        options: {
+            config: '<%= package.configs %>/compass/config.rb',
+            basePath: '<%= package.resources %>',
+            cssDir: '<%= package.prototype %>/css',
+            noLineComments: true,
+            outputStyle: 'compressed',
+            environment: 'production'
+        }
+
+    },
 };
