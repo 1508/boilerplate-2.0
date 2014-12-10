544e87a4ee6992285c74e452999e3c927f72d548
Rune <rkh@1508.dk>
2014-12-10 15:45:10 +0100

    Added compression to boilerplate

--- Interface/grunttasks/sass.js
+++ Interface/grunttasks/sass.js
@@ -1,22 +1,30 @@
 module.exports = {
-    prototype: {
-        sourceMap: true,
-        imagePath: '<%= package.prototyperesources %>/images',
+    prototype: { 
+        options:{
+            sourceMap: true,
+            outputStyle: 'nested',
+            imagePath: '<%= package.prototyperesources %>/images'
+        },
         files: {
             '<%= package.prototyperesources %>/css/styles.css': '<%= package.resources %>/sass/styles.scss'
         }
     },
     development: {
-        sourceMap: true,
-        imagePath: '<%= package.buildresources %>/images',
+        options:{
+            sourceMap: true,
+            outputStyle: 'nested',
+            imagePath: '<%= package.buildresources %>/images'
+        },
         files: {
             '<%= package.buildresources %>/css/styles.css': '<%= package.resources %>/sass/styles.scss'
         }
     },
     production: {
-        sourceMap: false,
-        outputStyle: 'compressed',
-        imagePath: '<%= package.buildresources %>/images',
+        options:{
+            sourceMap: false,
+            outputStyle: 'compressed',
+            imagePath: '<%= package.buildresources %>/images'
+        },
         files: {
             '<%= package.buildresources %>/css/styles.css': '<%= package.resources %>/sass/styles.scss'
         }
